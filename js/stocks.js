07122851df6235c598bda72f9e5f7d8a8bd0b4bb
MathewsOwen <matheusowenowen@gmail.com>
2026-03-16 11:38:22 -0300

    Update stocks.js

--- js/stocks.js
+++ js/stocks.js
@@ -1,1 +1,18 @@
+const stocks=[
+{symbol:"PETR4",price:"38.50"},
+{symbol:"VALE3",price:"66.20"},
+{symbol:"ITUB4",price:"31.90"},
+{symbol:"WEGE3",price:"45.10"}
+]
+
+const table=document.getElementById("stocksTable")
+
+if(table){
+table.innerHTML=stocks.map(s=>`
+
+<div class="marketRow">
+${s.symbol} - ${s.price}
+</div>
+`).join("")
+}
 
